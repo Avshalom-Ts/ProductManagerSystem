5467135a94bc57ffd3458d8eafb19b4ab1dbdab5
Azdevpp <Azdevpp@gmail.com>
2023-01-06 16:35:22 +0200

    Genereting the view component and implamented the routh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
+import { ViewProductComponent } from './view-product/view-product.component';
 
 @NgModule({
-  declarations: [AppComponent, AddProductComponent],
+  declarations: [AppComponent, AddProductComponent, ViewProductComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
